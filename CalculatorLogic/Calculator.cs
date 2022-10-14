bdde53b51e170a22df8de137a98534e60072d9a2
David Bankole Abel <ballack_davidabel@yahoo.com>
2022-10-14 15:39:05 -0400

    moving my files between folder in github

---
+++ CalculatorLogic/Calculator.cs
@@ -0,0 +1,37 @@
+﻿namespace CalculatorLogic;
+
+public static class Calculator
+{   //Adding method of 2 numbers
+    public static int Add(int left, int right)
+    {
+        return left + right;
+    }
+    //subtracting method of 2 numbers
+    public static double SubstractNumbers(double left, double right)
+    {
+        //return subtracted numbers
+        return left - right;
+    }
+
+    //multiplying method of 2 numbers
+    public static double MultiplyNumbers(double left, double right)
+    {
+        //return mutliplied numbers
+        return left * right;
+    }
+
+    //division method of 2 numbers
+    public static double DivideNumbers(double left, double right)
+    {
+        //checked if the 2nd number is zero so that you can catch the error
+        //if not make the division
+        if (left != 0)
+        {
+            return left / right;
+        }
+        else
+        {
+            throw new DivideByZeroException();
+        }
+    }
+}
