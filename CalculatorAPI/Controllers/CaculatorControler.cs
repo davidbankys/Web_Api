7f3521ffd9756cc96b18bfb825be28fe5fbeec01
David Bankole Abel <ballack_davidabel@yahoo.com>
2022-10-14 19:22:06 -0400

    added logics but its not working

---
+++ CalculatorAPI/Controllers/CaculatorControler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorAPI.Controllers
+{
+    //here defines the api route via the api of each api method
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpPost]
+        public double Add(double right, double left)
+        {
+            //using the simple calculator logic class to add 2 numbers
+            double result = CalculatorLogic.Calculator.AddNumbers(right, left);
+
+            //return added values in 2 decimal points
+            return Math.Round(result, 2);
+        }
+
+        [HttpPost]
+        public double Substract(double num1, double num2)
+        {
+            //using the simple calculator logic class to substract 2 numbers
+            double result = CalculatorLogic.Calculator.SubstractNumbers(right, left);
+
+            return Math.Round(result, 2);
+        }
+
+        [HttpPost]
+        public double Multiply(double num1, double num2)
+        {
+            //using the simple calculator logic class to multiply 2 numbers
+            double result = CalculatorLogic.Calculator.MultiplyNumbers(right, left);
+
+            //return added values in 2 decimal points
+            return Math.Round(result, 2);
+        }
+
+        [HttpPost]
+        public double Divide(double left, double right)
+        {
+            double result = 0;
+
+            //I am using the try and catch method here to catch any exception error throw from diving any number by zero
+            try
+            {
+                //using the  calculator logic class to divide 2 numbers
+                result = CalculatorLogic.Calculator.DivideNumbers(left, right);
+            }
+            catch (DivideByZeroException)
+            {
+                //using dividebyzeo exception to catch specific error and changing status code to 400
+                Response.StatusCode = 400;
+            }
+
+            return Math.Round(result, 2);
+        }
+    }
+}
--- CalculatorTest/UnitTest1.cs
+++ CalculatorTest/UnitTest1.cs
@@ -12,4 +12,168 @@ public class UCalculatorTest
         int result = Calculator.Add(1, 1);
         Assert.AreEqual(2, result);
     }
+
+    [TestMethod]
+    public void Add_A_Number_And_Variable_To_Get_A_Positive_Number_Since_Variable_Defaults_To_Zero()
+    {
+        var result = CalculatorLogic.Calculator.Add(1, 32);
+        Assert.AreEqual(33, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Add_Two_Negative_Whole_Numbers_Correctly_And_Return_A_Double()
+    {
+        var result = CalculatorLogic.Calculator.Add(-2, -2);
+        Assert.AreEqual(-4, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Add_One_Positive_And_Negative_Correctly_And_Return_A_Double()
+    {
+        var result = CalculatorLogic.Calculator.Add(22, -36);
+        Assert.AreEqual(-14, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Add_One_Positive_Double_And__Positive_Whole_Number_Correctly_And_Return_A_Double()
+    {
+        var result = CalculatorLogic.Calculator.Add((int)2.4, 20);
+        Assert.AreEqual(22.4, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+
+    [TestMethod]
+    public void Add_One_Positive_Double_And_Negative_Whole_Number_Correctly_And_Return_A_Double()
+    {
+        var result = CalculatorLogic.Calculator.Add((int)5.5, -30);
+        Assert.AreEqual(-24.5, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+
+    [TestMethod]
+    public void Add_Two_Zero_Numbers_And_Return_A_Zero()
+    {
+        var result = CalculatorLogic.Calculator.Add(0, 0);
+        Assert.AreEqual(0, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+
+    [TestMethod]
+    public void Add_Two_Negative_Decimal_Numbers_Correctly_And_Return_Negative()
+    {
+        var result = CalculatorLogic.Calculator.Add((int)-2.2, (int)-4.2);
+        Assert.AreEqual(-6.4, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Add_One_Positive_And_Zero_Correctly_And_Return_A_Double()
+    {
+        var result = CalculatorLogic.Calculator.Add(0, 5);
+        Assert.AreEqual(5, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Add_Positve_2_Point_1()
+    {
+        var result = CalculatorLogic.Calculator.Add((int)2.1, (int)1.1);
+        Assert.AreEqual(3.2, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    
+    //Subtraction logic tests
+    //
+
+    [TestMethod]
+    public void Subtract_Two_Positive_Whole_Number_Correctly_And_Return_A_Positive_Double()
+    {
+        var result = CalculatorLogic.Calculator.SubstractNumbers(13, 12);
+        Assert.AreEqual(1, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Subtract_One_Number_And_Variable_To_Get_An_Negative_Number_Since_Variable_Defaults_To_Zero()
+    {
+        var result = CalculatorLogic.Calculator.SubstractNumbers(ed, 32);
+        Assert.AreEqual(-32, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Subtract_Two_Negative_Whole_Numbers_Correctly_And_Return_A_Negative_Nubmber()
+    {
+        var result = CalculatorLogic.Calculator.SubstractNumbers(-22, -2);
+        Assert.AreEqual(-20, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Subtract_One_Positive_And_Negative_Correctly_And_Return_A_Positive_Double()
+    {
+        var result = CalculatorLogic.Calculator.SubstractNumbers(22, -452);
+        Assert.AreEqual(474, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Subtract_One_Positive_Decimal_And_Positive_Whole_Number_Correctly_And_Return_A_Negative()
+    {
+        var result = CalculatorLogic.Calculator.SubstractNumbers(2.4, 20);
+        Assert.AreEqual(-17.6, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+
+    [TestMethod]
+    public void Subtract_One_Positive_Decimal_And_Negative_Whole_Number_Correctly_And_Return_A_Double()
+    {
+        var result = CalculatorLogic.Calculator.SubstractNumbers(5.5, -30);
+        Assert.AreEqual(35.5, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+
+    [TestMethod]
+    public void Subtract_Two_Zero_Numbers_And_Return_A_Zero()
+    {
+        var result = CalculatorLogic.Calculator.SubstractNumbers(0, 0);
+        Assert.AreEqual(0, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+
+    [TestMethod]
+    public void Subtract_Two_Negative_Decimal_Numbers_Correctly_And_Return_Positive()
+    {
+        var result = CalculatorLogic.Calculator.SubstractNumbers(-2.2, -4.2);
+        Assert.AreEqual(2.0, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Subtract_One_Positive_And_Zero_Correctly_And_Return_A_Negative_Double()
+    {
+        var result = CalculatorLogic.Calculator.SubstractNumbers(0, 5);
+        Assert.AreEqual(-5, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Subtract_Two_Positve_Whole_Numbers()
+    {
+        var result = CalculatorLogic.Calculator.SubstractNumbers(2.1, 1.1);
+        Assert.AreEqual(1.0, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+
 }
