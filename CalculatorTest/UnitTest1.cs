ec47e5e29b67b958a8cebc3f08af5a8684c33eb6
David Bankole Abel <ballack_davidabel@yahoo.com>
2022-10-14 15:51:06 -0400

    issues with my git has been fixed .. logic added

---
+++ CalculatorTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿
+using CalculatorLogic;
+namespace CalculatorTest;
+
+
+[TestClass]
+public class UCalculatorTest
+{
+    [TestMethod]
+    public void Add_Two_Positive_Integer()
+    {
+        int result = Calculator.Add(1, 1);
+        Assert.AreEqual(2, result);
+    }
+}
